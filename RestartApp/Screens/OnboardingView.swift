9dcffb901dc09b809780ceb0e11d1a92f77ed524
abdWasiq <abdul.wasiq@naheed.pk>
2022-03-30 12:58:27 +0500

    initialist OnboardinScreen with simple TapGesture

--- RestartApp/Screens/OnboardingView.swift
+++ RestartApp/Screens/OnboardingView.swift
@@ -13,16 +13,92 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     //MARK: BODY
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding View")
-                .font(.largeTitle)
-            Button(action: {
-                isOnboardingViewActive = false //changing userdefault value
-            }) {
-                Text("Start")
-            }
-
-        } //: VStack
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                //MARK: Header
+                Spacer()
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    Text("""
+                        It's not how much we give but
+                        how much love we put into givig.
+                        """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                    
+                    
+                } //: - HEADER
+                
+                //MARK: Center
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: - Center
+                Spacer()
+                //MARK: Footer
+                
+                ZStack {
+                    //Parts of the custom Button
+                    
+                    //1. background (static)
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding()
+                    //2. call to action (static)
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    //3. Capsule (Dynamic Button)
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 90) // Setting Width 90, because of parent size
+                        Spacer()
+                    }
+                    //4. Circle (Draggable)
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding()
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 90, height:90, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        Spacer()
+                    }
+                    //4. Circle ZStack..
+                }//: - FOOTER
+                .frame(height: 90, alignment: .center).padding()
+            }// -VStack
+        } // -ZStack
     }
 }
 
